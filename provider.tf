e72129741c62adc2a20494706da7f7daf0c703b9
rampradyumnarampalli <rampradyumnarampalli@gmail.com>
2022-01-09 11:08:18 +0000

    updated secret

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA4VYPEDEP2UVLKIP3"
-  secret_key = "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"
+  access_key = "AKIA4VYPEDEPYWWNWUEO"
+  secret_key = "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"
 }
